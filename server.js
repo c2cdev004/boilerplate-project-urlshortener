657d564150990477428f0dc105797e925dd610f4
TranVanXuat <xuattv@evolableasia.vn>
2019-08-05 12:56:06 +0700

    URL Shortener Microservice

--- server.js
+++ server.js
@@ -3,13 +3,41 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 var cors = require('cors');
 
 var app = express();
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
+
+
+const dns = require('dns');
+const { URL } = require('url');
+
+
+mongoose.connect('mongodb+srv://vanxuat:canhavuive12@cluster0-culpo.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true });
+
+
+const urlSave = require('./model/db');
+app.use(bodyParser.urlencoded({extended: false}));
+const urlShort =function(url, callback) {
+  
+  let longUrl = null;
+  let err = null;
+  try {
+    longUrl = new URL(url);
+  } catch (err) {
+    return callback(err, null);
+  }
+  dns.lookup(longUrl.hostname, (err, address, family) => {
+    if (err) {
+      return callback(err, null);
+    }
+    return callback(null, longUrl.origin);
+  }); 
+  
+};
 
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.MONGOLAB_URI);
@@ -31,7 +59,28 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.post('/api/shorturl/new',(req,res)=>{
+  
+   const newLongUrl = req.body.url;
+  urlShort(newLongUrl, function (err, url) {
+    if (err) {
+      console.error("Error: Url didn't validate:");
+      console.error(err);      
+      res.json({error: 'invalid url'});      
+    } else {      
+      urlSave.createNew(url, function(err, shortUrl) {
+        if (err) {
+          console.error('Error: fail with database');
+          console.error(err);
+          res.send('Error with database.');
+        } else {
+          res.json({original_url: url, short_url: shortUrl});
+        }
+      });
+    }
+  });
+  
+});
 app.listen(port, function () {
   console.log('Node.js listening ...');
 });
